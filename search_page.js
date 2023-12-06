38ee8be7ea79b07a10b4492c3091321cfa168995
ariuna221 <63195700+ariuna221@users.noreply.github.com>
2023-12-06 11:49:17 +0800

    a
    aa

---
+++ search_page.js
@@ -0,0 +1,45 @@
+function searchData() {
+    var searchTerm = document.getElementById('searchTerm').value;
+
+    // Make an AJAX request to searchdata.php
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.responseText); 
+            if (xhr.status === 200) {
+                var resultContainer = document.getElementById('resultContainer');
+
+                // Parse the JSON response
+                var data = JSON.parse(xhr.responseText);
+
+                // Process the data and update the result container
+                displayResults(data, resultContainer);
+            } else {
+                console.error('Error:', xhr.status, xhr.statusText);
+            }
+        }
+    };
+
+    // Send a GET request with the search term
+    xhr.open('GET', 'searchdata.php?searchTerm=' + encodeURIComponent(searchTerm), true);
+    xhr.send();
+}
+
+function displayResults(data, container) {
+    // Clear previous results
+    container.innerHTML = '';
+
+    if (data.length > 0) {
+        // If there are results, create a list and append to the container
+        var list = document.createElement('ul');
+        data.forEach(function (item) {
+            var listItem = document.createElement('li');
+            listItem.textContent = item.description;
+            list.appendChild(listItem);
+        });
+        container.appendChild(list);
+    } else {
+        // If no results, display a message
+        container.textContent = 'No results found.';
+    }
+}
---
+++ searchdata.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "127.0.0.1";
+$port = 3307;
+$username = "root";
+$password = "123";
+$dbname = "itu";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the search term from the GET request
+$searchTerm = $_GET['searchTerm'];
+
+// Replace 'YourTableName' with the actual table name
+$tableName = 'UsingInfo';
+
+// Perform the search query
+$sql = "SELECT * FROM $UsingInfo WHERE description LIKE '%$searchTerm%'";
+$result = $conn->query($sql);
+
+// Check for errors in the query
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
+
+// Process the results into an array
+$data = [];
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+// Close the database connection
+$conn->close();
+
+// Send the results in regular JSON format if no errors occurred
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
